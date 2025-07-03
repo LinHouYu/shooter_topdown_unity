f7a48d089def4d43b3b36ecd033a399835ac183f
知识份子 <zhishi_fenzi@hotmail.com>
2025-07-03 12:47:12 -0300

    枪的音效

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -1,7 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
+    private AudioSource GunAudio;
+    float fireRate = 0.5f; //射击间隔时间
+    private void Awake()
+    {
+        //获取玩家的音频源组件
+        GunAudio = GetComponent<AudioSource>();
+        if (GunAudio == null)
+        {
+            Debug.LogError("PlayerShooting脚本需要附加在一个带有AudioSource组件的GameObject上。");
+        }
+    }
     void Update()
     {
         //获取用户的开火按键
@@ -22,6 +34,8 @@ public class PlayerShooting : MonoBehaviour
     {
         //在这里实现射击逻辑
         Debug.Log("Player is shooting!");
+        Debug.Log(DateTime.Now.ToString("G"));
+        GunAudio.Play();
         //可以添加射击特效、音效等
         //例如：Instantiate(bulletPrefab, transform.position, transform.rotation);
     }
